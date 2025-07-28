41e9eaa1ccf10c3edda89393ab4c3d53192aa500
Rodrigo Reis <rr199820031@gmail.com>
2025-07-28 23:44:33 +0100

    Lockscreen : Change symbol to RR

--- dwm/slock/config.h
+++ dwm/slock/config.h
@@ -3,7 +3,7 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[INIT] =   "white",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
@@ -17,18 +17,32 @@ static const int logosize = 25;
 static const int logow = 12;
 static const int logoh = 6;
 
-static XRectangle rectangles[9] = {
+static XRectangle rectangles[] = {
 	/* x    y       w       h */
-	{ 0,    3,      1,      3 },
-	{ 1,    3,      2,      1 },
-	{ 0,    5,      8,      1 },
-	{ 3,    0,      1,      5 },
-	{ 5,    3,      1,      2 },
-	{ 7,    3,      1,      2 },
-	{ 8,    3,      4,      1 },
-	{ 9,    4,      1,      2 },
-	{ 11,   4,      1,      2 },
+	{ 0,    0,      3,      1 },
+	{ 0,    2,      1,      2 },
+	{ 3,    1,      1,      1 },
+	{ 2,    2,      1,      1 },
+	{ 3,    3,      1,      1 },
+
+	{ 5+0,    0,      3,      1 },
+	{ 5+0,    2,      1,      2 },
+	{ 5+3,    1,      1,      1 },
+	{ 5+2,    2,      1,      1 },
+	{ 5+3,    3,      1,      1 },
+
+	// { 4,    3,      1,      3 },
+	// { 1,    3,      2,      1 },
+	// { 0,    5,      8,      1 },
+	// { 3,    0,      1,      5 },
+	// { 5,    3,      1,      2 },
+	// { 7,    3,      1,      2 },
+	// { 8,    3,      4,      1 },
+	// { 9,    4,      1,      2 },
+	// { 11,   4,      1,      2 },
 };
+
+
 
 /*Enable blur*/
 #define BLUR
